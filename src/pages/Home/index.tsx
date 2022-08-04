b1e3500610c7091240705cef1f9b8ddae022487b
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-08-04 13:53:18 -0300

    feat: sign up form

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleSubmit() {}
+
+  return (
+    <div className="container">
+      <div className="container-form">
+        <h1 className="title">Create account</h1>
+        <input
+          type="text"
+          className="input mb"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="email"
+          className="input"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <button
+          type="button"
+          className="btn mt"
+          onClick={handleSubmit}
+          disabled={name === "" || email === ""}
+        >
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+}
