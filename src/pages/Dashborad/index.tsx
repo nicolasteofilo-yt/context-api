074e8518040adcdc4c612599cdf60878e71c485c
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-08-05 17:54:52 -0300

    feat: implementation authContext in dashboard page

--- src/pages/Dashborad/index.tsx
+++ src/pages/Dashborad/index.tsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom'
+
+import { AuthContext } from '../../context/AuthContext';
+
 import './styles.css';
 
 export default function Dashboard() {
+    const { user } = useContext(AuthContext);
+
     return (
         <div className='container-dash'>
-            <h1 className="name"><span className='hello'>Olá</span> Nicolas</h1>
-            <span className='email'><b>Email: </b>nicolasteste@gmail.com</span>
-            <Link to="/" className='btn-go-back'>Voltar para Home</Link>
+            <h1 className="name"><span className='hello'>Olá</span> {user.name ? user.name : 'User not found'}</h1>
+            <span className='email'><b>Email: </b>{user.email ? user.email : 'E-mail not found'}</span>
+            <Link to="/" className='btn-go-back'>Go back to Home</Link>
         </div>
     )
 }
