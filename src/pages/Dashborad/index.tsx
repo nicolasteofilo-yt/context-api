149a2e33bed20a36c87581b8ad8888756be10547
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-08-05 17:55:18 -0300

    fix: update text in button

--- src/pages/Dashborad/index.tsx
+++ src/pages/Dashborad/index.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
         <div className='container-dash'>
             <h1 className="name"><span className='hello'>Olá</span> Nicolas</h1>
             <span className='email'><b>Email: </b>nicolasteste@gmail.com</span>
-            <Link to="/" className='btn-go-back'>Voltar para Home</Link>
+            <Link to="/" className='btn-go-back'>Go back to Home</Link>
         </div>
     )
 }
